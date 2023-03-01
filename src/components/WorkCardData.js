7e92ba3d6baad3b8499a030bb4e01d8f05bc5300
Swastik-Babu <117106234+Swastik-Babu@users.noreply.github.com>
2023-03-01 23:59:03 +0530

    Updated workdata with suvichar

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -8,18 +8,21 @@ const ProjectCardData=[
     title:"Calculation of Carbon emiison of a code snippet",
     text: "THERE was NO PROPER FRAMEWORK WHICH CALCULATES THE CARBON EMISSION OF A CODE SNIPPET IN A GIVEN LANGUAGE.TO CALCULATE THE AMOUNT OF CO2 EMISSIONS THAT A SNIPPET PRODUCED.TO PROVIDE SUGGESTIONS THAT HELPED TO OPTIMISE THE GIVEN CODE THEREBY REDUCING THE CARBON FOOTPRINT.",
     view:"https://github.com/Swastik-Babu/Swastik-Babu-Portfolio",
+    source:"https://github.com/Swastik-Babu/Swastik-Babu-Portfolio",
    },
    {
     imgsrc:pro2,
-    title:"Notes To Note",
+    title:"Suvichar",
     text: "MY very own notes app.",
-    view:"https://github.com/Swastik-Babu/Swastik-Babu-Portfolio",
+    view:"https://suvichar.netlify.app/",
+    source:"https://github.com/Swastik-Babu/suvichar",
    },
    {
     imgsrc:pro3,
     title:"Daily Task Tracker",
     text: "To do list app where you can keep a track of your daily activities ",
     view:"https://github.com/Swastik-Babu/Task-manager-Daily-Tracker",
+    source:"https://github.com/Swastik-Babu/Task-manager-Daily-Tracker"
    }   
 ];
 export default ProjectCardData
